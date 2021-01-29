685ccdf1d95d4bdd9bc73eb43ce9c1ee5daac884
Bridget Shannon <bridgetrshannon@gmail.com>
2021-01-29 13:51:52 -0500

    package.json file

--- js/scripts.js
+++ js/scripts.js
@@ -52,17 +52,3 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
-
-var message = "";
-
-$("#sendMessage").on("click", function () {
-  message = $("#contactform").serialize();
-  $.ajax({
-    url: "//formspree.io/bridgetrshannon@gmail.com",
-    method: "POST",
-    data: { message: message },
-    dataType: "json",
-  });
-  alert("Thanks for the email, we'll be in touch promptly.");
-  return false;
-});
